5a93c80b049a15b28f0d6bd9e59ca5345a4515bf
Alberto Maianti <albertomaianti@MacBook-Pro-di-Alberto.local>
2015-05-04 17:42:27 +0200

    Added first scraping

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cheerio = require('cheerio');
+var $ = require('cheerio');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
 app.post('/parse/', function(request, response) {
-	console.log(request.body);
+	//console.log(request.body);
+
+	console.log(extractEmails(request.body.mess));
+
+	var email = $.load(request.body.mess);
+	email('table').map(function(i, foo){
+		foo = $(foo);
+		console.log('**** ' + foo.html());
+	});
+
 	var res = {"error":false,"emailParsed":""};
 	response.send(res);
 });
@@ -20,3 +31,9 @@ app.post('/parse/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+function extractEmails (text)
+{
+    return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+}
