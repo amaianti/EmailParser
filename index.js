59bd13c2afcce1645a4dc6e33c941b3231392377
Alberto Maianti <albertomaianti@MacBook-Pro-di-Alberto.local>
2015-05-04 16:25:03 +0200

    Added bodyparser

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var cheerio = require('cheerio');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser());
+app.use(bodyParser());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
